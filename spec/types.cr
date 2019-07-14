a7fb4867ab5c5fd50d416e13dd18b527fe5e137b
Szekeres Tamás <szektam2@gmail.com>
2019-07-14 15:58:56 +0200

    Add types for testing Serializable module

--- spec/types.cr
+++ spec/types.cr
@@ -16,12 +16,38 @@ end
 alias Union1 = Bool | UInt16 | Int32
 alias Union2 = (UInt8 | String)?
 
+# Simple struct
 struct Point
   include SSZ::Serializable
 
-  property x : Int32
-  property y : Int32
+  property x : Int32 = 0
+  property y : Int32 = 0
 
   def initialize(@x : Int32 = 0, @y : Int32 = 0)
   end
 end
+
+module DbEntry
+  property id : UInt64
+end
+
+class Person
+  include SSZ::Serializable
+
+  property name : String = ""
+  property age : UInt16 = 0_u16
+
+  def initialize(@name : String, @age : UInt16)
+  end
+
+  def ==(other : Person) : Bool
+    (@name == other.name) && (@age == other.age)
+  end
+end
+
+# Inherited
+# class Employee < Person
+#   include SSZ::Serializable
+
+#   property company : String
+# end
