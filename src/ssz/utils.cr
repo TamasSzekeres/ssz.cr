29902eda0cbaf39d202da66034ee814ba5b3d9c3
Szekeres Tamás <szektam2@gmail.com>
2019-07-24 21:15:59 +0200

    Add `Slice(T).resize` function

--- src/ssz/utils.cr
+++ src/ssz/utils.cr
@@ -13,4 +13,20 @@ struct Slice(T)
     other.copy_to(dst.to_unsafe + size, other.size)
     dst
   end
+
+  # Resizes slice to `new_size`-size.
+  # If new size if bigger than actual size sets new elemets to `value`.
+  #
+  # ```
+  # Bytes[1, 2].resize(4, 0_u8) # => Bytes[1, 2, 0, 0]
+  # Bytes[1, 2, 3, 4].resize(2, 0_u8) # => Bytes[1, 2]
+  # ```
+  def resize(new_size : Int32, value : T) : self
+    case size <=> new_size
+    when 1 then self[0...new_size]
+    when -1 then self + Slice(T).new(new_size - size, value)
+    else
+      self
+    end
+  end
 end
