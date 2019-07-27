dc92f9eaa60b32ecb982fce05922142f72a1959c
Szekeres Tamás <szektam2@gmail.com>
2019-07-27 20:12:52 +0200

    Add `UUID` coding

--- src/ssz/codec.cr
+++ src/ssz/codec.cr
@@ -1,3 +1,5 @@
+require "uuid"
+
 require "./constants"
 
 class Object
@@ -512,6 +514,52 @@ struct Union
     {% end %}
 
     raise "Invalid input!"
+  end
+end
+
+struct UUID
+  def self.ssz_basic? : Bool
+    false
+  end
+
+  def ssz_basic? : Bool
+    false
+  end
+
+  def self.ssz_variable? : Bool
+    true
+  end
+
+  def ssz_variable? : Bool
+    true
+  end
+
+  def self.ssz_fixed? : Bool
+    false
+  end
+
+  def ssz_fixed? : Bool
+    false
+  end
+
+  def ssz_size : Int32
+    to_s.ssz_size
+  end
+
+  def ssz_encode : Bytes
+    to_s.ssz_encode
+  end
+
+  def ssz_encode(io : IO)
+    to_s.ssz_encode(io)
+  end
+
+  def self.ssz_decode(io : IO, size : Int32)
+    UUID.new(String.ssz_decode(io, size))
+  end
+
+  def self.ssz_decode(bytes : Bytes)
+    ssz_decode(IO::Memory.new(bytes), bytes.size)
   end
 end
 
