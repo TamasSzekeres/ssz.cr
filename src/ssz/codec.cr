f849798c173a21db7689917f1ead19c1de20ef67
Szekeres Tamás <szektam2@gmail.com>
2019-07-27 20:49:43 +0200

    Add `Time` coding

--- src/ssz/codec.cr
+++ src/ssz/codec.cr
@@ -517,6 +517,52 @@ struct Union
   end
 end
 
+struct Time
+  def self.ssz_basic? : Bool
+    false
+  end
+
+  def ssz_basic? : Bool
+    false
+  end
+
+  def self.ssz_variable? : Bool
+    false
+  end
+
+  def ssz_variable? : Bool
+    false
+  end
+
+  def self.ssz_fixed? : Bool
+    true
+  end
+
+  def ssz_fixed? : Bool
+    true
+  end
+
+  def ssz_size : Int32
+    sizeof(Int64)
+  end
+
+  def ssz_encode : Bytes
+    to_unix_ms.ssz_encode
+  end
+
+  def ssz_encode(io : IO)
+    to_unix_ms.ssz_encode(io)
+  end
+
+  def self.ssz_decode(io : IO, size : Int32)
+    Time.unix_ms(Int64.ssz_decode(io, size))
+  end
+
+  def self.ssz_decode(bytes : Bytes)
+    ssz_decode(IO::Memory.new(bytes), bytes.size)
+  end
+end
+
 struct UUID
   def self.ssz_basic? : Bool
     false
