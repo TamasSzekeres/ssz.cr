8c9bb42b23d77fc4f5dfe6bee23384d79cd42832
Szekeres Tamás <szektam2@gmail.com>
2019-07-27 20:58:30 +0200

    Add `Time::Span` coding

--- src/ssz/codec.cr
+++ src/ssz/codec.cr
@@ -563,6 +563,56 @@ struct Time
   end
 end
 
+struct Time::Span
+  def self.ssz_basic? : Bool
+    false
+  end
+
+  def ssz_basic? : Bool
+    false
+  end
+
+  def self.ssz_variable? : Bool
+    false
+  end
+
+  def ssz_variable? : Bool
+    false
+  end
+
+  def self.ssz_fixed? : Bool
+    true
+  end
+
+  def ssz_fixed? : Bool
+    true
+  end
+
+  def ssz_size : Int32
+    sizeof(Int64) + sizeof(Int32)
+  end
+
+  def ssz_encode : Bytes
+    to_i.ssz_encode + nanoseconds.ssz_encode
+  end
+
+  def ssz_encode(io : IO)
+    to_i.ssz_encode(io)
+    nanoseconds.ssz_encode(io)
+  end
+
+  def self.ssz_decode(io : IO, size : Int32)
+    Time::Span.new(
+      seconds: Int64.ssz_decode(io, sizeof(Int64)),
+      nanoseconds: Int32.ssz_decode(io, sizeof(Int32))
+    )
+  end
+
+  def self.ssz_decode(bytes : Bytes)
+    ssz_decode(IO::Memory.new(bytes), bytes.size)
+  end
+end
+
 struct UUID
   def self.ssz_basic? : Bool
     false
