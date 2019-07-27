05a44248f5ca11993d5bdc2c1fc1b1b62e1d9e3b
Szekeres Tamás <szektam2@gmail.com>
2019-07-27 19:07:13 +0200

    Add `URI` coding

--- src/ssz/codec.cr
+++ src/ssz/codec.cr
@@ -726,3 +726,49 @@ struct BigRational
     ssz_decode(IO::Memory.new(bytes), bytes.size)
   end
 end
+
+class URI
+  def self.ssz_basic? : Bool
+    false
+  end
+
+  def ssz_basic? : Bool
+    false
+  end
+
+  def self.ssz_variable? : Bool
+    true
+  end
+
+  def ssz_variable? : Bool
+    true
+  end
+
+  def self.ssz_fixed? : Bool
+    false
+  end
+
+  def ssz_fixed? : Bool
+    false
+  end
+
+  def ssz_size : Int32
+    to_s.ssz_size
+  end
+
+  def ssz_encode : Bytes
+    to_s.ssz_encode
+  end
+
+  def ssz_encode(io : IO)
+    to_s.ssz_encode(io)
+  end
+
+  def self.ssz_decode(io : IO, size : Int32)
+    URI.parse(String.ssz_decode(io, size))
+  end
+
+  def self.ssz_decode(bytes : Bytes)
+    ssz_decode(IO::Memory.new(bytes), bytes.size)
+  end
+end
